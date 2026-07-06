547e5b8998b9eaf1469cd260c76b5e1e7853fd53
Diane Bury <benjamin.folie@student.isae-supaero.fr>
2026-07-06 13:27:13 +0200

    rosbag

--- real_commander/launch/sinus_mvt.launch.py
+++ real_commander/launch/sinus_mvt.launch.py
@@ -95,8 +95,8 @@ def generate_launch_description():
         name='sinus_mvt',
         output='screen',
         parameters=[{
-            "AMPLITUDE" : 0.2,
-            "OMEGA" : 3.,
+            "AMPLITUDE" : 0.1,
+            "OMEGA" : 1.,
             "LOOP_FREQ" :250,
             }]
         )
